7d0276e11e7dd5bda5cf1e8144aa751145fe3c21
John Soo <jsoo1@asu.edu>
2018-12-02 21:25:22 -0800

    Build sdump

--- yaft.scm
+++ yaft.scm
@@ -5,50 +5,48 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix download)
   #:use-module (guix git-download)
-  #:use-module ((guix licenses) #:prefix license)
   #:use-module (guix packages)
+  #:use-module (libsixel)
   #:export (yaft
             idump
-            ;; TODO
-            ;; sdump
-            ))
+            sdump))
 
 (define yaft
   (package
-   (name "yaft")
-   (version "0.2.9")
-   (source (origin
-            (method url-fetch)
-            (uri (string-append "https://github.com/uobikiemukot/yaft/archive/v" version ".tar.gz"))
-            (sha256 (base32 "0k1pn1fph7zinb44mb0bdrlmvi93irplss4whmvlz8zhgy9ydxw0"))))
-   (build-system gnu-build-system)
-   (inputs `(("gcc" ,gcc)))
-   (arguments
-    `(#:make-flags (list (string-append "DESTDIR=" (assoc-ref %outputs "out")))
-      #:tests? #f
-      #:phases
-      (modify-phases
-       %standard-phases
-       (delete 'configure)
-       (add-before 'build 'make-locale
-                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                     (setenv "LANG" "en_US.UTF-8")
-                     (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
-                     (setenv "DESTDIR" (assoc-ref outputs "out"))
-                     #t))
-       (add-after 'install 'postinstall
-                  (lambda* (#:key outputs #:allow-other-keys)
-                    (let ((out (assoc-ref outputs "out")))
-                      (copy-recursively (string-append out "/usr/bin") (string-append out "/bin"))
-                      (copy-recursively (string-append out "/usr/share") (string-append out "/share"))
-                      (delete-file-recursively (string-append out "/usr"))
-                      #t))))))
-   (inputs `(("ncurses" ,ncurses)))
-   (outputs '("out"))
-   (synopsis "Yet another framebuffer terminal")
-   (home-page "https://github.com/uobikiemukot/yaft")
-   (description
-    "Yet Another Framebuffer Terminal (aka \"yaft\") is simple terminal emulator for minimalist.
+    (name "yaft")
+    (version "0.2.9")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/uobikiemukot/yaft/archive/v" version ".tar.gz"))
+              (sha256 (base32 "0k1pn1fph7zinb44mb0bdrlmvi93irplss4whmvlz8zhgy9ydxw0"))))
+    (build-system gnu-build-system)
+    (inputs `(("gcc" ,gcc)))
+    (arguments
+     `(#:make-flags (list (string-append "DESTDIR=" (assoc-ref %outputs "out")))
+       #:tests? #f
+       #:phases
+       (modify-phases
+           %standard-phases
+         (delete 'configure)
+         (add-before 'build 'make-locale
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (setenv "LANG" "en_US.UTF-8")
+             (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+             (setenv "DESTDIR" (assoc-ref outputs "out"))
+             #t))
+         (add-after 'install 'postinstall
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((out (assoc-ref outputs "out")))
+               (copy-recursively (string-append out "/usr/bin") (string-append out "/bin"))
+               (copy-recursively (string-append out "/usr/share") (string-append out "/share"))
+               (delete-file-recursively (string-append out "/usr"))
+               #t))))))
+    (inputs `(("ncurses" ,ncurses)))
+    (outputs '("out"))
+    (synopsis "Yet another framebuffer terminal")
+    (home-page "https://github.com/uobikiemukot/yaft")
+    (description
+     "Yet Another Framebuffer Terminal (aka \"yaft\") is simple terminal emulator for minimalist.
 
 Features:
 
@@ -59,46 +57,87 @@ Features:
     wallpaper
     DRCS (DECDLD/DRCSMMv1) (experimental)
     sixel (experimental)")
-   ;; TODO: Fix license
-   (license #f)))
+    ;; TODO: Fix license
+    (license #f)))
 
 (define idump
   (let ((commit "f3d0da4ac1675604ccb14d9f34887777d2e20181")
         (revision "1"))
     (package
-     (name "idump")
-     (version (git-version "v0.2.0" revision commit))
-     (source
-      (origin
-       (method git-fetch)
-       (uri (git-reference (url "https://github.com/uobikiemukot/idump")
-                           (commit commit)))
-       (file-name (git-file-name name version))
-       (sha256 (base32 "1h29hp67ynmwffnskkhrnrp8xq8ac6cg1qipqx51zp1b6znnlcvw"))))
-     (build-system gnu-build-system)
-     (inputs `(("gcc" ,gcc)
-               ("libjpeg" ,libjpeg)
-               ("libpng" ,libpng)
-               ("libtiff" ,libtiff)))
-     (arguments
-      `(#:tests? #f
-        #:phases
-        (modify-phases %standard-phases
-                       (delete 'configure)
-                       (delete 'install)
-                       (add-before 'build 'make-locale
-                                   (lambda* (#:key inputs outputs #:allow-other-keys)
-                                     (setenv "LANG" "en_US.UTF-8")
-                                     (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
-                                     (setenv "DESTDIR" (assoc-ref outputs "out"))
-                                     #t))
-                       (add-after 'build 'jank-install
-                                  (lambda* (#:key outputs #:allow-other-keys)
-                                    (let ((out (assoc-ref outputs "out")))
-                                          (install-file "idump" (string-append out "/bin"))
-                                          #t))))))
-     ;; TODO: fix license
-     (home-page "https://github.com/uobikiemukot/idump")
-     (synopsis "tiny image viewer for framebuffer")
-     (description "")
-     (license #f))))
+      (name "idump")
+      (version (git-version "v0.2.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/uobikiemukot/idump")
+                             (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256 (base32 "1h29hp67ynmwffnskkhrnrp8xq8ac6cg1qipqx51zp1b6znnlcvw"))))
+      (build-system gnu-build-system)
+      (inputs `(("gcc" ,gcc)
+                ("libjpeg" ,libjpeg)
+                ("libpng" ,libpng)
+                ("libtiff" ,libtiff)))
+      (arguments
+       `(#:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure)
+           (delete 'install)
+           (add-before 'build 'make-locale
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (setenv "LANG" "en_US.UTF-8")
+               (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+               (setenv "DESTDIR" (assoc-ref outputs "out"))
+               #t))
+           (add-after 'build 'jank-install
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let ((out (assoc-ref outputs "out")))
+                 (install-file "idump" (string-append out "/bin"))
+                 #t))))))
+      ;; TODO: fix license
+      (home-page "https://github.com/uobikiemukot/idump")
+      (synopsis "tiny image viewer for framebuffer")
+      (description "")
+      (license #f))))
+
+(define sdump
+  (let ((commit "c5fb55b6c2e548fcd229b75a9ff091cdc3b2e230")
+        (revision "1"))
+    (package
+      (name "sdump")
+      (version (git-version "0.1.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference (url "https://github.com/uobikiemukot/sdump.git")
+                             (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "00grshn14dk7jjkmcylnn2ral815fsqwhf9w1vaiwhwh90qpbbh6"))))
+      (build-system gnu-build-system)
+      (inputs `(("libjpeg" ,libjpeg)
+                ("libpng" ,libpng)
+                ("libsixel" ,libsixel)))
+      (arguments
+       `(#:tests? #f
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure)
+           (delete 'install)
+           (add-before 'build 'make-locale
+             (lambda* (#:key inputs outputs #:allow-other-keys)
+               (setenv "LANG" "en_US.UTF-8")
+               (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+               (setenv "DESTDIR" (assoc-ref outputs "out"))
+               #t))
+           (add-after 'build 'jank-install
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let ((out (assoc-ref outputs "out")))
+                 (install-file "sdump" (string-append out "/bin"))
+                 #t))))))
+      (home-page "https://github.com/uobikiemukot/sdump")
+      (synopsis "sixel image dumper")
+      (description "this program is a variant of idump")
+      (license #f))))
+
