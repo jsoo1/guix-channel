7d28561362473f498c2220a30a7d15bb6b6f72c7
John Soo <jsoo@panosoft.com>
2019-05-04 12:14:32 -0700

    More work on pyside

--- python-pyside.scm
+++ python-pyside.scm
@@ -2,14 +2,25 @@
   #:use-module ((gnu packages base) #:select (which))
   #:use-module ((gnu packages cmake) #:select (cmake))
   #:use-module ((gnu packages version-control) #:select (git))
-  #:use-module ((gnu packages llvm) #:select (clang))
+  #:use-module ((gnu packages llvm) #:select (clang-6 libcxx llvm-6))
   #:use-module ((gnu packages python) #:select (python-wrapper python-2))
   #:use-module ((gnu packages python-xyz) #:select (python-wheel))
   #:use-module ((gnu packages qt)
-                #:select (qt-4
+                #:select (qt
                           qtbase
+                          qtdatavis3d
                           qtmultimedia
+                          qtquickcontrols
+                          qtscript
+                          qtscxml
+                          qtsensors
+                          qtspeech
+                          qtsvg
                           qttools
+                          qtwebchannel
+                          qtwebkit
+                          qtwebsockets
+                          qtx11extras
                           qtxmlpatterns))
   #:use-module ((gnu packages xml) #:select (libxml2 libxslt))
   #:use-module (guix build-system cmake)
@@ -18,7 +29,9 @@
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module ((guix licenses) #:prefix license:)
-  #:export (python-pyside-2
+  #:export (libcxx-6
+            python-apiextractor-2
+            python-pyside-2
             python-pyside-2-tools
             python-apiextractor-2
             python-shiboken-2))
@@ -39,43 +52,46 @@
          (sha256
           (base32
            "0g8jacm2iqd7lw2m7f1dp1nnrsk38bl3m8pihm8zz9gxs8d31sf5"))))
-      (build-system python-build-system)
+      (build-system cmake-build-system)
       (inputs
-       `(("libxml2" ,libxml2)
+       `(("clang-6" ,clang-6)
+         ("libcxx-6" ,libcxx-6)
+         ("libxml2" ,libxml2)
          ("libxslt" ,libxslt)
-         ("python-wheel" ,python-wheel)))
-      (native-inputs
-       `(("clang" ,clang)
-         ("cmake" ,cmake)
-         ("git" ,git)
+         ("llvm-6" ,llvm-6)
          ("qtbase" ,qtbase)
+         ("qtdatavis3d" ,qtdatavis3d)
          ("qtmultimedia" ,qtmultimedia)
+         ("qtquickcontrols" ,qtquickcontrols) ;; ??
+         ("qtscript" ,qtscript)
+         ("qtscxml" ,qtscxml)
+         ("qtsensors" ,qtsensors)
+         ("qtspeech" ,qtspeech)
+         ("qtsvg" ,qtsvg)
          ("qttools" ,qttools)
-         ("qtxmlpatterns" ,qtxmlpatterns)
+         ("qtwebchannel" ,qtwebchannel)
+         ("qtwebsockets" ,qtwebsockets)
+         ("qtx11extras" ,qtx11extras)))
+      (native-inputs
+       `(("cmake" ,cmake)
+         ("git" ,git)
+         ("python-shiboken-2" ,python-shiboken-2)
+         ("python-wrapper" ,python-wrapper)
          ("which" ,which)))
       (arguments
        `(#:tests? #f
-         #:use-setuptools? #f
+         #:configure-flags
+         '("-DBUILD_TESTS=off"
+           "-DSANITIZE_ADDRESS=on")
          #:phases
-         (modify-phases %standard-phases
-           (add-before 'build 'fix-build-paths
+         (modify-phases
+             %standard-phases
+           (add-after 'unpack 'go-to-source-dir
+             (lambda _ (chdir "sources/pyside2") #t))
+           (add-before 'configure 'set-clang-dir
              (lambda* (#:key inputs #:allow-other-keys)
-               (let ((clang (assoc-ref inputs "clang"))
-                     (libxml2 (assoc-ref inputs "libxml2"))
-                     (libxslt (assoc-ref inputs "libxslt")))
-                 (substitute* "sources/shiboken2/CMakeLists.txt"
-                   (("\\$\\{CLANG_DIR\\}")
-                    (string-append (assoc-ref inputs "clang"))))
-                 (substitute* "sources/pyside2/CMakeLists.txt"
-                   (("include\\(FindQt5Extra\\)") ""))
-                 (setenv "LIBXML2_LIBRARY"
-                         (string-append libxml2 "/lib"))
-                 (setenv "LIBXML2_INCLUDE_DIR"
-                         (string-append libxml2 "/include"))
-                 (setenv "LIBXSLT_LIBRARIES"
-                         (string-append libxslt "/lib"))
-                 (setenv "LIBXSLT_INCLUDE_DIR"
-                         (string-append libxslt "/include"))
+               (let ((clangdir (assoc-ref inputs "clang-6")))
+                 (setenv "CLANG_INSTALL_DIR" clangdir)
                  #t))))))
       (home-page "https://wiki.qt.io/Qt_for_Python")
       (synopsis
@@ -91,36 +107,18 @@
          ;; https://code.qt.io/cgit/pyside/pyside-setup.git/tree/LICENSE.GPLv3-EXCEPT
          ;; https://code.qt.io/cgit/pyside/pyside-setup.git/tree/LICENSE.COMMERCIAL
          license:lgpl3
-         license:bsd3 ; pyside-tools
          )))))
 
-(define python-pyside-2-tools
-  (let ((revision "1")
-        (commit "f1b775537e7fbd718516749583b2abf1cb6adbce"))
-    (package
-      (name "python-pyside-2-tools")
-      (version (git-version "v5.11.2" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://code.qt.io/pyside/pyside-tools")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "0g8jacm2iqd7lw2m7f1dp1nnrsk38bl3m8pihm8zz9gxs8d31sf5"))))
-      (build-system cmake-build-system)
-      (arguments
-       `(#:tests? #f
-         #:configure-flags '("-DBUILD_TESTS=off")))
-      (home-page "https://wiki.qt.io/Qt_for_Python")
-      (synopsis
-       "lupdate, rcc and uic tools for PySide2")
-      (description
-       "TODO")
-      ;; TODO: understand (this is per the arch repo)
-      (license license:lgpl2.1+))))
+(define libcxx-6
+  (package
+    (inherit libcxx)
+    (name "libcxx-6")
+    (version (package-version llvm-6))
+    (source
+     (inherit (package-source libcxx))
+     (sha256
+      (base32
+       "1wdrxg365ig0kngx52pd0n820sncp24blb0zpalc579iidhh4002")))))
 
 (define python-shiboken-2
   (let ((revision "1")
@@ -140,20 +138,26 @@
            "0g8jacm2iqd7lw2m7f1dp1nnrsk38bl3m8pihm8zz9gxs8d31sf5"))))
       (build-system cmake-build-system)
       (inputs
-       `(("clang" ,clang)
+       `(("clang-6" ,clang-6)
          ("libxml2" ,libxml2)
          ("libxslt" ,libxslt)
+         ("llvm-6" ,llvm-6)
          ("python-wrapper" ,python-wrapper)
-         ("python-wheel" ,python-wheel)
          ("qtbase" ,qtbase)
          ("qtxmlpatterns" ,qtxmlpatterns)))
       (arguments
        `(#:tests? #f
          #:configure-flags '("-DBUILD_TESTS=off")
          #:phases
-         (modify-phases %standard-phases
+         (modify-phases
+             %standard-phases
            (add-after 'unpack 'use-shiboken-dir-only
-             (lambda* _ (chdir "sources/shiboken2") #t)))))
+             (lambda _ (chdir "sources/shiboken2") #t))
+           (add-before 'configure 'set-clang-dir
+             (lambda* (#:key inputs #:allow-other-keys)
+               (let ((clangdir (assoc-ref inputs "clang-6")))
+                 (setenv "CLANG_INSTALL_DIR" clangdir)
+                 #t))))))
       (home-page "https://wiki.qt.io/Qt_for_Python")
       (synopsis
        "Shiboken generates bindings for C++ libraries using CPython source code")
@@ -164,95 +168,33 @@
        '(license:gpl2+
          license:lgpl)))))
 
-(define python-shiboken
+(define python-pyside-2-tools
   (let ((revision "1")
-        (commit "09e1ffb01597b6fe4ed1ba1ae362449e3780d7ed"))
+        (commit "f1b775537e7fbd718516749583b2abf1cb6adbce"))
     (package
-      (name "python-shiboken")
-      (version (git-version "1.2.4" revision commit))
+      (name "python-pyside-2-tools")
+      (version (git-version "v5.11.2" revision commit))
       (source
        (origin
          (method git-fetch)
-         (uri (git-reference (url "https://github.com/pyside/Shiboken")
-                             (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "0adsnbrsiiip4cvh67c69k3b24brm7xr0pmigsd3agydj1bd9lyx"))))
-      (build-system cmake-build-system)
-      (inputs
-       `(("libxml2" ,libxml2)
-         ("libxslt" ,libxslt)
-         ("python-2.7" ,python-2.7)
-         ("python-generatorrunner" ,python-generatorrunner)
-         ("python-wheel" ,python-wheel)
-         ("qt-4" ,qt-4)))
-      (arguments
-       `(#:tests? #f
-         #:phases
-         (modify-phases %standard-phases
-           (add-before 'configure 'use-guix-generatorrunner
-             (lambda* (#:key inputs #:allow-other-keys)
-               (let ((generatorrunner (assoc-ref "python-generatorrunner" inputs)))
-                 (substitute* "generator/CMakeLists.txt"
-                   (("\\$\\{GENERATORRUNNER_PLUGIN_DIR\\}")
-                    (string-append generatorrunner "lib/generatorrunner")))
-                 #t))))))
-      (home-page "http://www.pyside.org")
-      (synopsis
-       "Shiboken generates bindings for C++ libraries using CPython source code")
-      (description
-       "Shiboken generates bindings for C++ libraries using CPython source code")
-      (license #f))))
-
-(define python-generatorrunner
-  (let ((revision "1")
-        (commit "29bfdca3d2fd6f87fc15f5e24fdeb532281855b2"))
-    (package
-      (name "python-generatorrunner")
-      (version (git-version "0.6.17" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference (url "https://code.qt.io/cgit/pyside/generatorrunner.git")
-                             (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "1sv9xcbd8fxs2mq0g07m1xncx9c6wb8z9izcv05shir8wwdaj9gx"))))
-      (build-system cmake-build-system)
-      (inputs
-       `(("python-apiextractor" ,python-apiextractor)
-         ("qt-4" ,qt-4)))
-      (home-page "http://www.pyside.org")
-      (synopsis "Generate Qt and C++ bindings for Python")
-      (description
-       "GeneratorRunner is the application responsible for loading and executing the generator front-ends provided by the user as plug-ins.")
-      (license license:gpl2+))))
-
-(define python-apiextractor
-  (let ((revision "1")
-        (commit "25b23b4f039650c6e5d64592e6e7210719e52097"))
-    (package
-      (name "python-apiextractor")
-      (version (git-version "0.10.11" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference ;; (url "https://github.com/pyside/Apiextractor")
-               (url "https://code.qt.io/cgit/pyside/apiextractor.git")
+         (uri (git-reference
+               (url "https://code.qt.io/pyside/pyside-tools")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
           (base32
-           "1kgrzb3hv3xaj6li97kw4simj4jhli2kvzw61ahcwvrhc27flhwi"))))
+           "1w2g5afvww9r89wmdm9jx8sz67x4bzy9difkh72n4c73ya1n91ry"))))
       (build-system cmake-build-system)
       (inputs
-       `(("libxml2" ,libxml2)
-         ("libxslt" ,libxslt)
-         ("qt-4" ,qt-4)))
-      (home-page "http://www.pyside.org")
-      (synopsis "")
+       `(("python-shiboken-2" ,python-shiboken-2)
+         ("qtbase" ,qtbase)))
+      (arguments
+       `(#:tests? #f
+         #:configure-flags '("-DBUILD_TESTS=off")))
+      (home-page "https://wiki.qt.io/Qt_for_Python")
+      (synopsis
+       "lupdate, rcc and uic tools for PySide2")
       (description
-       "API Extractor collects information about libraries from its header files and merges it with user written files (typesystem) describing how the library should be exposed as a binding on the target language.")
-      (license license:gpl2+))))
+       "TODO")
+      ;; TODO: understand (this is per the arch repo)
+      (license license:lgpl2.1+))))
