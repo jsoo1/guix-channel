61ba7f5595543fd20eb83b9c78d4f4a6114ed73e
John Soo <jsoo@panosoft.com>
2019-08-11 18:17:16 -0700

    Shorten description

--- cedille.scm
+++ cedille.scm
@@ -89,26 +89,7 @@ Cedille, terms are nothing more than annotated versions of terms
 of pure untyped lambda calculus.  In contrast, in Coq or Agda,
 the typing annotations are intrinsic parts of terms.  The typing
 annotations can only be erased as an optimization under certain
-conditions, not by virtue of the definition of the type theory.
-
-Cedille’s type theory allows one to derive inductive datatypes,
-together with their induction principles.  These derivations are
-done via lambda-encodings, including not just the familiar Church
-encoding (with its well-known limitation to inefficient accessors),
-but also more efficient Parigot and Mendler encodings.  Further,
-Cedille supports datatype declarations and pattern-matching
-recursion via elaboration to certain of these encodings.
-
-Cedille is used from an emacs mode, which communicates with the
-backend tool.  The emacs mode supports convenient navigation of
-the source text following the structure of its syntax tree.  Typing
-and context information is available for all subexpressions as
-one navigates, as well as information related to type inference.
-  Cedille implements a novel form of local type inference called
-spine-local type inference.  At the moment this is restricted to
-solving for first-order type variables, but in the coming 2018-2019
-academic year we plan to add support for inferring values for term
-variables as well as dependent and higher-order type variables.")
+conditions, not by virtue of the definition of the type theory.")
     (license license:expat)))
 
 (define emacs-se-mode
