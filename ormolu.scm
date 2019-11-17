1f829c3b9e20cb2caae343cd95e8fafdf86148df
John Soo <jsoo1@asu.edu>
2019-11-16 21:52:06 -0800

    Add ormolu.

---
+++ ormolu.scm
@@ -0,0 +1,41 @@
+(define-module (ormolu)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system haskell)
+  #:use-module (gnu packages haskell-xyz)
+  #:use-module (gnu packages haskell-check)
+  #:use-module ((guix licenses) #:prefix license:))
+
+(define-public ormolu
+  (package
+    (name "ormolu")
+    (version "0.0.1.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append
+               "https://hackage.haskell.org/package/ormolu/ormolu-"
+               version
+               ".tar.gz"))
+        (sha256
+          (base32
+            "1p4m9hiavirnhf941fb5pdnrlrknr5rhcvznhqywianvvw6qcm30"))))
+    (build-system haskell-build-system)
+    (inputs
+      `(("ghc-dlist" ,ghc-dlist)
+        ("ghc-exceptions" ,ghc-exceptions)
+        ("ghc-paths" ,ghc-paths)
+        ("ghc-syb" ,ghc-syb)
+        ("ghc-gitrev" ,ghc-gitrev)
+        ("ghc-optparse-applicative" ,ghc-optparse-applicative)))
+    (native-inputs
+      `(("ghc-hspec" ,ghc-hspec)
+        ("ghc-path" ,ghc-path)
+        ("ghc-path-io" ,ghc-path-io)
+        ("hspec-discover" ,hspec-discover)))
+    (home-page "https://github.com/tweag/ormolu")
+    (synopsis "A formatter for Haskell source code")
+    (description
+      "A formatter for Haskell source code.")
+    (license license:bsd-3)))
+
