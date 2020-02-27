864e398550c66177a44527108e5e34e49adc2f0e
John Soo <jsoo1@asu.edu>
2020-02-27 08:57:46 -0800

    Add tealdeer.

--- rust-xyz.scm
+++ rust-xyz.scm
@@ -10624,3 +10624,466 @@ for Rust.")
       "Manage your personal zoo of repositories.")
     (license license:cc0)))
 
+(define-public rust-utime-0.2
+  (package
+    (name "rust-utime")
+    (version "0.2.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "utime" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0akr32rd2qiiwdbnkwd6hs4zfy76791xlqgwh9h5cp515iamhl05"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-kernel32-sys" ,rust-kernel32-sys-0.2)
+         ("rust-libc" ,rust-libc-0.2)
+         ("rust-winapi" ,rust-winapi-0.3))))
+    (home-page
+      "https://github.com/simnalamburt/utime")
+    (synopsis "Missing utime function for Rust")
+    (description
+      "This package provides a missing utime function for Rust.")
+    (license (list license:asl2.0 license:expat))))
+
+(define-public rust-treeline-0.1
+  (package
+    (name "rust-treeline")
+    (version "0.1.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "treeline" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0hcdgyk5xzcx2ylm0fr9czzs9cjznm7l9q5qz51qi97i82r43xx7"))))
+    (build-system cargo-build-system)
+    (home-page
+      "https://github.com/softprops/treeline")
+    (synopsis "Visualize tree structured data")
+    (description "A library for visualizing tree structured data.")
+    (license license:expat)))
+
+(define-public rust-predicates-core-1.0
+  (package
+    (name "rust-predicates-core")
+    (version "1.0.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "predicates-core" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0y3ingf2i4xx7r61f1a8wizs57j8hh32hylyjbw9ymcj7qx5q1q6"))))
+    (build-system cargo-build-system)
+    (home-page
+      "https://github.com/assert-rs/predicates-rs/tree/master/predicates-core")
+    (synopsis
+      "API for boolean-valued predicate functions.")
+    (description
+      "An API for boolean-valued predicate functions.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-predicates-tree-1.0
+  (package
+    (name "rust-predicates-tree")
+    (version "1.0.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "predicates-tree" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "090148qjilm2c722l873z7g31fhzj5j4qhd2xiv8mcqkj22w8qwf"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-predicates-core" ,rust-predicates-core-1.0)
+         ("rust-treeline" ,rust-treeline-0.1))))
+    (home-page
+      "https://github.com/assert-rs/predicates-rs/tree/master/predicates-tree")
+    (synopsis
+      "Render boolean-valued predicate functions results as a tree")
+    (description
+      "Render boolean-valued predicate functions results as a tree.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-normalize-line-endings-0.3
+  (package
+    (name "rust-normalize-line-endings")
+    (version "0.3.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "normalize-line-endings" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "1gp52dfn2glz26a352zra8h04351icf0fkqzw1shkwrgh1vpz031"))))
+    (build-system cargo-build-system)
+    (home-page
+      "https://github.com/derekdreery/normalize-line-endings")
+    (synopsis
+      "Turn an iterator over chars and into one with line endings")
+    (description
+      "Takes an iterator over chars and returns a new iterator with
+all line endings.")
+    (license license:asl2.0)))
+
+(define-public rust-normalize-line-endings-0.2
+  (package
+    (inherit rust-normalize-line-endings-0.3)
+    (version "0.2.2")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "normalize-line-endings" version))
+        (file-name
+          (string-append (package-name rust-normalize-line-endings-0.3)
+                         "-" version ".tar.gz"))
+        (sha256
+          (base32
+           "1a1knz9j1w5a1pl2q6whmjphm3z6p64r5njnam7syp5rx8wil2if"))))))
+
+(define-public rust-float-cmp-0.6
+  (package
+    (name "rust-float-cmp")
+    (version "0.6.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "float-cmp" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0zb1lv3ga18vsnpjjdg87yazbzvmfwwllj3aiid8660rp3qw8qns"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-num-traits" ,rust-num-traits-0.2))))
+    (home-page
+      "https://github.com/mikedilger/float-cmp")
+    (synopsis
+      "Floating point approximate comparison traits")
+    (description
+      "Floating point approximate comparison traits.")
+    (license license:expat)))
+
+(define-public rust-predicates-1.0
+  (package
+    (name "rust-predicates")
+    (version "1.0.3")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "predicates" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "12nsna7zrxf0vygzlvq4ib0i79m8d7091cc3qcl85hc15h4vz20i"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-difference" ,rust-difference-2.0)
+         ("rust-float-cmp" ,rust-float-cmp-0.6)
+         ("rust-normalize-line-endings"
+          ,rust-normalize-line-endings-0.2)
+         ("rust-predicates-core" ,rust-predicates-core-1.0)
+         ("rust-regex" ,rust-regex-1.3))
+        #:cargo-development-inputs
+        (("rust-predicates-tree" ,rust-predicates-tree-1.0))))
+    (home-page
+      "https://github.com/assert-rs/predicates-rs")
+    (synopsis
+      "Implementation of boolean-valued predicate functions")
+    (description
+      "An implementation of boolean-valued predicate functions.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-assert-fs-0.13
+  (package
+    (name "rust-assert-fs")
+    (version "0.13.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "assert_fs" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "1gzi4s5k5bmd4l9a0vmiank2iflzik4dlv3k4skrpa3lkyhlfnw1"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-doc-comment" ,rust-doc-comment-0.3)
+         ("rust-globwalk" ,rust-globwalk-0.7)
+         ("rust-predicates" ,rust-predicates-1.0)
+         ("rust-predicates-core" ,rust-predicates-core-1.0)
+         ("rust-predicates-tree" ,rust-predicates-tree-1.0)
+         ("rust-tempfile" ,rust-tempfile-3.1))))
+    (home-page
+      "https://github.com/assert-rs/assert_fs")
+    (synopsis
+      "Filesystem fixtures and assertions for testing")
+    (description
+      "Filesystem fixtures and assertions for testing.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-escargot-0.5
+  (package
+    (name "rust-escargot")
+    (version "0.5.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "escargot" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0vd9phbpd6yrnsksn2as8flvq8ykzvck2zlz143xpp42qaz9dkvl"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-lazy-static" ,rust-lazy-static-1.4)
+         ("rust-log" ,rust-log-0.4)
+         ("rust-serde" ,rust-serde-1.0)
+         ("rust-serde-json" ,rust-serde-json-1.0))
+        #:cargo-development-inputs
+        (("rust-assert-fs" ,rust-assert-fs-0.13))))
+    (home-page
+      "https://github.com/crate-ci/escargot")
+    (synopsis "Cargo API written in Paris")
+    (description "Cargo API written in Paris.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-escargot-0.3
+  (package
+    (inherit rust-escargot-0.5)
+    (version "0.3.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "escargot" version))
+        (file-name
+         (string-append (package-name rust-escargot-0.5)
+                        "-" version ".tar.gz"))
+        (sha256
+          (base32
+           "19fmn7bz1h6nlqy0mp825xwjwnrjn4xjdpwc06jl51j3fiz1znqr"))))
+    (arguments
+      `(#:cargo-inputs
+        (("rust-lazy-static" ,rust-lazy-static-1.4)
+         ("rust-log" ,rust-log-0.4)
+         ("rust-serde" ,rust-serde-1.0)
+         ("rust-serde-json" ,rust-serde-json-1.0))
+        #:cargo-development-inputs
+        (("rust-assert-fs" ,rust-assert-fs-0.13))))))
+
+(define-public rust-assert-cmd-0.12
+  (package
+    (name "rust-assert-cmd")
+    (version "0.12.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "assert_cmd" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0k32njzxczq9x7r4fa3sp8gwm97y2rw77i0v946lf9kjvp4bm0v2"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-doc-comment" ,rust-doc-comment-0.3)
+         ("rust-escargot" ,rust-escargot-0.5)
+         ("rust-predicates" ,rust-predicates-1.0)
+         ("rust-predicates-core" ,rust-predicates-core-1.0)
+         ("rust-predicates-tree" ,rust-predicates-tree-1.0))))
+    (home-page
+      "https://github.com/assert-rs/assert_cmd")
+    (synopsis "Test CLI Applications")
+    (description "Test CLI Applications.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-assert-cmd-0.10
+  (package
+    (inherit rust-assert-cmd-0.12)
+    (version "0.10.2")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "assert_cmd" version))
+        (file-name
+          (string-append (package-name rust-assert-cmd-0.12)
+                         "-" version ".tar.gz"))
+        (sha256
+          (base32
+           "0rfc8mc13bd7zl3ha6klnqzcpjyfxpk2nd5phyxf9r3m1wk5rb5p"))))
+    (arguments
+      `(#:cargo-inputs
+        (("rust-doc-comment" ,rust-doc-comment-0.3)
+         ("rust-escargot" ,rust-escargot-0.5)
+         ("rust-predicates" ,rust-predicates-1.0)
+         ("rust-predicates-core" ,rust-predicates-core-1.0)
+         ("rust-predicates-tree" ,rust-predicates-tree-1.0))))))
+
+(define-public rust-pager-0.15
+  (package
+    (name "rust-pager")
+    (version "0.15.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "pager" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0a35mg68s0p63ya2k5hsg620c4llkjw2fx1sfi0laz4pz8myv75n"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-errno" ,rust-errno-0.2)
+         ("rust-libc" ,rust-libc-0.2))))
+    (home-page "https://gitlab.com/imp/pager-rs.git")
+    (synopsis
+      "Helps pipe your output through an external pager")
+    (description
+      "Helps pipe your output through an external pager.")
+    (license (list license:asl2.0 license:expat))))
+
+(define-public rust-shell32-sys-0.1
+  (package
+    (name "rust-shell32-sys")
+    (version "0.1.2")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "shell32-sys" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0b0jjnv8rr4flllvpx9lkfvvxdl3hac53cd2klf14mqz2134pq4y"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-winapi" ,rust-winapi-0.3))
+        #:cargo-development-inputs
+        (("rust-winapi-build" ,rust-winapi-build-0.1))))
+    (home-page
+      "https://github.com/retep998/winapi-rs")
+    (synopsis
+      "Function definitions for the Windows API library shell32")
+    (description
+      "Contains function definitions for the Windows API library
+shell32.  See winapi for types and constants.")
+    (license license:expat)))
+
+(define-public rust-ole32-sys-0.2
+  (package
+    (name "rust-ole32-sys")
+    (version "0.2.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "ole32-sys" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "134xg38xicrqynx4pfjfxnpp8x83m3gqw5j3s8y27rc22w14jb2x"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-winapi" ,rust-winapi-0.3))
+        #:cargo-development-inputs
+        (("rust-winapi-build" ,rust-winapi-build-0.1))))
+    (home-page
+      "https://github.com/retep998/winapi-rs")
+    (synopsis
+      "Function definitions for the Windows API library ole32")
+    (description
+      "Contains function definitions for the Windows API library
+ole32.  See winapi for types and constants.")
+    (license license:expat)))
+
+(define-public rust-app-dirs-1.2
+  (package
+    (name "rust-app-dirs")
+    (version "1.2.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "app_dirs" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "0g9ijvl4xqbmahy7d7s0wj570wg0kz3ad0jk77b98smxv6x28fp7"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:cargo-inputs
+        (("rust-ole32-sys" ,rust-ole32-sys-0.2)
+         ("rust-shell32-sys" ,rust-shell32-sys-0.1)
+         ("rust-winapi" ,rust-winapi-0.2)
+         ("rust-xdg" ,rust-xdg-2.2))))
+    (home-page
+      "https://github.com/AndyBarron/app-dirs-rs")
+    (synopsis
+      "Put your app's data in the right place on every platform")
+    (description
+      "Put your app's data in the right place on every platform.")
+    (license license:expat)))
+
+(define-public rust-strsim-0.7
+  (package
+    (inherit rust-strsim-0.9)
+    (name "rust-strsim")
+    (version "0.7.0")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "strsim" version))
+        (file-name (string-append name "-" version ".crate"))
+        (sha256
+         (base32
+          "0l7mkwvdk4vgnml67b85mczk466074aj8yf25gjrjslj4l0khkxv"))))))
+
+(define-public rust-docopt-1.0
+  (package
+    (inherit rust-docopt-1.1)
+    (version "1.0.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "docopt" version))
+       (file-name
+        (string-append (package-name rust-docopt-1.1)
+                       "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "09djlzwkdmmic3jfinr29v25v0x8m6b8fchyzh3p4nwvaz10cafv"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-lazy-static" ,rust-lazy-static-1)
+        ("rust-regex" ,rust-regex-1.1)
+        ("rust-serde" ,rust-serde-1.0)
+        ("rust-strsim" ,rust-strsim-0.7))))))
---
+++ tealdeer.scm
@@ -0,0 +1,74 @@
+(define-module (tealdeer)
+  #:use-module (gnu packages crates-io)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages rust)
+  #:use-module (guix build-system cargo)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (rust-xyz))
+
+(define-public tealdeer
+  (package
+    (name "tealdeer")
+    (version "1.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "tealdeer" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0j0zs3hzk7871j29ppb1skq31r624j89f2lng895mmf28wfk7pfh"))))
+    (build-system cargo-build-system)
+    (inputs `(("openssl" ,openssl)))
+    (arguments
+     `(#:tests? #f
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'configure 'dont-vendor-sources
+           (lambda* (#:key inputs #:allow-other-keys)
+             (let ((openssl (assoc-ref inputs "openssl")))
+               (setenv "OPENSSL_DIR" openssl))
+             #t))
+         (add-after 'install 'install-completions
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out")))
+               (install-file
+                "bash_tealdeer"
+                (string-append out "/etc/bash_completion.d"))
+               (install-file
+                "fish_tealdeer"
+                (string-append
+                 out "/share/completions/vendor_completions.d"))))))
+       #:cargo-inputs
+       (("rust-ansi-term" ,rust-ansi-term-0.12)
+        ("rust-app-dirs" ,rust-app-dirs-1.2)
+        ("rust-docopt" ,rust-docopt-1.0)
+        ("rust-env-logger" ,rust-env-logger-0.7)
+        ("rust-flate2" ,rust-flate2-1.0)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-miniz-oxide-c-api" ,rust-miniz-oxide-c-api-0.2)
+        ("rust-pager" ,rust-pager-0.15)
+        ("rust-reqwest" ,rust-reqwest)
+        ("rust-serde" ,rust-serde-1.0)
+        ("rust-serde-derive" ,rust-serde-derive-1.0)
+        ("rust-tar" ,rust-tar-0.4)
+        ("rust-toml" ,rust-toml-0.5)
+        ("rust-walkdir" ,rust-walkdir-2.2)
+        ("rust-xdg" ,rust-xdg-2.2))
+       #:cargo-development-inputs
+       (("rust-assert-cmd" ,rust-assert-cmd-0.10)
+        ("rust-escargot" ,rust-escargot-0.3)
+        ("rust-predicates" ,rust-predicates-1.0)
+        ("rust-tempdir" ,rust-tempdir-0.3)
+        ("rust-utime" ,rust-utime-0.2))))
+    (home-page "https://github.com/dbrgn/tealdeer/")
+    (synopsis
+     "Fetch and show tldr help pages for many CLI commands")
+    (description
+     "Fetch and show tldr help pages for many CLI commands.  Full
+featured offline client with caching support.")
+    (license (list license:expat license:asl2.0))))
