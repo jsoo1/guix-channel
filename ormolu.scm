87645de8cee55ee1cd56ce879ab1a6f2832fbae1
John Soo <jsoo1@asu.edu>
2021-10-19 18:31:55 -0700

    Update ormolu to be inline with upstreams ghc@8.10

--- ormolu.scm
+++ ormolu.scm
@@ -6,10 +6,33 @@
   #:use-module (gnu packages haskell-check)
   #:use-module ((guix licenses) #:prefix license:))
 
+(define ghc-lib-parser-9
+  (package
+    (name "ghc-lib-parser")
+    (version "9.0.1.20210324")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://hackage.haskell.org/package/ghc-lib-parser/ghc-lib-parser-"
+             version
+             ".tar.gz"))
+       (sha256
+        (base32 "1hl20lz8r9yhgh6hlakmbqp637h49mq3sdxciy4vsz3nhzy7nqqm"))))
+    (build-system haskell-build-system)
+    (native-inputs
+     `(("ghc-alex" ,ghc-alex)
+       ("ghc-happy" ,ghc-happy)))
+    (home-page "https://github.com/digital-asset/ghc-lib")
+    (synopsis "The GHC API, decoupled from GHC versions")
+    (description
+     "A package equivalent to the @ghc@ package, but which can be loaded on many compiler versions.")
+    (license license:bsd-3)))
+
 (define-public ormolu
   (package
     (name "ormolu")
-    (version "0.0.1.0")
+    (version "0.2.0.0")
     (source
      (origin
        (method url-fetch)
@@ -19,11 +42,13 @@
              ".tar.gz"))
        (sha256
         (base32
-         "1p4m9hiavirnhf941fb5pdnrlrknr5rhcvznhqywianvvw6qcm30"))))
+         "0zivz7vcl4m1rjay5md6cdqac9cnfwz9c844l20byiz5h49bwfhb"))))
     (build-system haskell-build-system)
     (inputs
-     `(("ghc-dlist" ,ghc-dlist)
+     `(("ghc-diff" ,ghc-diff)
+       ("ghc-dlist" ,ghc-dlist)
        ("ghc-exceptions" ,ghc-exceptions)
+       ("ghc-lib-parser" ,ghc-lib-parser-9)
        ("ghc-paths" ,ghc-paths)
        ("ghc-syb" ,ghc-syb)
        ("ghc-gitrev" ,ghc-gitrev)
